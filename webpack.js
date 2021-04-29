984edb726d57ba2965d43ea50ffd0bdd3c71c706
hlemoen <hlemoen@gmail.com>
2021-04-29 13:53:58 +0200

    testing_profil_meny

--- webpack.js
+++ webpack.js
@@ -1,3 +1,23 @@
-const webpackConfig = require('@nextcloud/webpack-vue-config')
+// const webpackConfig = require('@nextcloud/webpack-vue-config')
 
-module.exports = webpackConfig
+// module.exports = webpackConfig
+
+const path = require('path')
+
+module.exports = {
+	entry: {
+		dashboard: path.join(__dirname, 'src', 'dashboard'),
+		'user-status-menu': path.join(__dirname, 'src', 'main-user-status-menu'),
+	},
+	output: {
+		path: path.resolve(__dirname, './js'),
+		publicPath: '/js/',
+		filename: '[name].js?v=[chunkhash]',
+		jsonpFunction: 'webpackJsonpUserStatus',
+	},
+	optimization: {
+		splitChunks: {
+			automaticNameDelimiter: '-',
+		},
+	},
+}
